f4fa498f9b2698b7184d0a6a01515cc07b00566c
titabo2k <5295004+titabo2k@users.noreply.github.com>
2021-09-04 12:26:13 +0200

    7ea04bf related modifications

--- tests/openrefine-wrench/test_openrefine_wrench.py
+++ tests/openrefine-wrench/test_openrefine_wrench.py
@@ -1,5 +1,29 @@
 from context import openrefine_wrench
 
+wanted = {
+    "columnWidths": None,
+    "encoding": "UTF-8",
+    "guessCellValueTypes": False,
+    "headerLines": None,
+    "header_lines": 1,
+    "ignoreLines": None,
+    "ignore_lines": -1,
+    "includeFileSources": False,
+    "limit": -1,
+    "linesPerRow": None,
+    "processQuotes": True,
+    "projectName": None,
+    "projectTags": None,
+    "recordPath": None,
+    "separator": "#",
+    "sheets": None,
+    "skipDataLines": None,
+    "skip_data_lines": 0,
+    "storeBlankCellsAsNulls": True,
+    "storeBlankRows": True,
+    "storeEmptyStrings": True,
+    "trimStrings": False}
+
 def test_prep_options():
     options = openrefine_wrench._prep_options(
         source_format="csv",
@@ -8,15 +32,4 @@ def test_prep_options():
         encoding=None,
         custom_options='{"encoding": "UTF-8", "separator": "#"}')
 
-    assert options == {
-        "encoding": "UTF-8",
-        "separator": "#",
-        "ignore_lines": -1,
-        "header_lines": 1,
-        "skip_data_lines": 0,
-        "limit": -1,
-        "store_blank_rows": True,
-        "guess_cell_value_types": False,
-        "process_quotes": True,
-        "store_blank_cells_as_nulls": True,
-        "include_file_sources": False}
+    assert options == wanted
